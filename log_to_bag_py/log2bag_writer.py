2f4a848defea76e806ca652146bdea9ed7788309
bob <dunwenqiang@gmail.com>
2023-07-04 14:11:06 +0800

    生成随机着色点云，发布，和记录bag文件；

--- log_to_bag_py/log2bag_writer.py
+++ log_to_bag_py/log2bag_writer.py
@@ -1,45 +1,77 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.serialization import serialize_message
-from std_msgs.msg import String
-
+from sensor_msgs.msg import PointCloud2, PointField
 import rosbag2_py
 
-class SimpleBagRecorder(Node):
+
+class Log2BagWriter(Node):
     def __init__(self):
-        super().__init__('simple_bag_recorder')
+        super().__init__('log2bag_writer')
         self.writer = rosbag2_py.SequentialWriter()
 
         storage_options = rosbag2_py._storage.StorageOptions(
-            uri='my_bag',
+            uri='log_bag',
             storage_id='mcap')
         converter_options = rosbag2_py._storage.ConverterOptions('', '')
         self.writer.open(storage_options, converter_options)
 
         topic_info = rosbag2_py._storage.TopicMetadata(
-            name='chatter',
-            type='std_msgs/msg/String',
+            name='log_radar',
+            type='sensor_msgs/msg/PointCloud2',
             serialization_format='cdr')
         self.writer.create_topic(topic_info)
+        return
 
-        self.subscription = self.create_subscription(
-            String,
-            'chatter',
-            self.topic_callback,
-            10)
-        self.subscription
+    # reference: https://blog.csdn.net/qq_44992157/article/details/130662947
+    def write_points_to_bag(self):
+        self.get_logger().info('write_points_to_bag')
+        msg = PointCloud2()
+        POINTS_COUNT = 10000
 
-    def topic_callback(self, msg):
-        self.writer.write(
-            'chatter',
-            serialize_message(msg),
-            self.get_clock().now().nanoseconds)
+        msg.header.frame_id = 'map'
+        msg.height = 1
+        msg.width = POINTS_COUNT
+        msg.is_dense = True
+        fields = [
+            PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
+            PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
+            PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
+            PointField(name='rgb', offset=12, datatype=PointField.UINT32, count=1)
+        ]
+        msg.point_step = 16
+        msg.fields = fields
+
+        import random
+        import numpy as np
+        import time
+        pub = self.create_publisher(PointCloud2, 'colored_point_cloud', 10)
+        while True:
+            time.sleep(1)
+
+            point_data = []
+            for i in range(POINTS_COUNT):
+                x, y, z = 10 * random.random(), 10 * random.random(), 10 * random.random()
+                r, g, b = 255 * random.random(), 255 * random.random(), 255 * random.random()
+                rgb = (int(r) << 16) | (int(g) << 8) | int(b)
+                point_data.append([x, y, z, rgb])
+            msg.data = np.asarray(point_data, dtype=np.float32).tobytes()
+            msg.header.stamp = self.get_clock().now().to_msg()
+
+            pub.publish(msg)
+            self.writer.write(
+                'log_radar',
+                serialize_message(msg),
+                self.get_clock().now().nanoseconds)
+        return
 
 
 def main(args=None):
     rclpy.init(args=args)
-    sbr = SimpleBagRecorder()
-    rclpy.spin(sbr)
+    log2bag = Log2BagWriter()
+    log2bag.write_points_to_bag()
+
+    rclpy.spin(log2bag)
     rclpy.shutdown()
 
 
